c9ae6d6f7723e89c18e204d465223f3ac00f8132
David Lopez <davidjlopez@comcast.net>
2019-09-21 02:57:11 +0000

    setup controllers and routes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,7 @@
 class GamesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
+ 
   def index
     @games = Game.all
   end
@@ -10,14 +11,54 @@ class GamesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
+    @game = current_user.games.create(game_params)
+    if @game.valid?
+      redirect_to root_path
+    else
+      render :new, status: :uprocessable_entity
+    end
+  end
+
+  def show
+    @game = Game.find_by_id(params[:id])
+    return render_not_found if @game.blank?
+  end
+
+  def edit
+    @game = Game.find_by_id(params[:id])
+      return render_not_found if @game.blank?
+      return render_not_found(:forbidden) if @game.user != current_user
+  end
+
+  def update
+    @game = Game.find_by_id(params[:id])
+    return render_not_found if @game.blank?
+    return render_not_found(:forbidden) if @game.user != current_user
+
+    @game.update_attributes(game_params)
+    if @game.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :uprocessable_entity
+    end
+  end
+
+  def destroy
+    @game = Game.find_by_id(params[:id])
+    return render_not_found if @game.blank?
+    return render_not_found(:forbidden) if @game.user != current_user
+    @game.destroy
     redirect_to root_path
   end
 
   private
 
   def game_params
-    params.require(:game).permit(:name, :description, :rating)
+    params.require(:game).permit(:name, :rating, :description,)
+  end
+
+  def render_not_found(status=:not_found)
+    render plain: "#{status.to_s.titleize}", status: status
   end
 
 end
