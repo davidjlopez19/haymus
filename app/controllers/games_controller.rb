42aa89c022907c6230804cf8baf683e7d68f9bea
David Lopez <davidjlopez@comcast.net>
2019-09-21 00:25:10 +0000

    added devise

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,23 @@
 class GamesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
     @games = Game.all
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    current_user.places.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :description, :rating)
   end
 
 end
